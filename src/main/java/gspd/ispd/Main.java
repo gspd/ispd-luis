6316359dc8c25874e65531e6d13ff90744e556e5
Luís Baldissera <luis.baldissera26@gmail.com>
2019-06-07 15:30:24 -0300

    Properties update

--- src/main/java/gspd/ispd/Main.java
+++ src/main/java/gspd/ispd/Main.java
@@ -7,13 +7,20 @@ package gspd.ispd;
 import gspd.ispd.gui.JPrincipal;
 import gspd.ispd.gui.LogExceptions;
 import gspd.ispd.gui.SplashWindow;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.PrintStream;
+import java.net.URL;
 import java.util.Locale;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.UIManager;
 
 /**
@@ -27,44 +34,58 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Locale.setDefault(new Locale("en", "US"));
-        if (args.length > 0) {
-            Terminal tel = new Terminal(args);
-            tel.executar();
-            System.exit(0);
-        } else {
-            ImageIcon image = new ImageIcon(Main.class.getResource("/images/Splash.gif"));
-            SplashWindow window = new SplashWindow(image);
-            window.setText("Copyright (c) 2010 - 2014 GSPD.  All rights reserved.");
-            window.setVisible(true);
-            //Exibir e armazenar erros durante execução:
-            LogExceptions logExceptions = new LogExceptions(null);
-            Thread.setDefaultUncaughtExceptionHandler(logExceptions);
-            // cria os novos fluxos de saida para arquivo
-            FileOutputStream fosErr = null;
-            FileOutputStream fosOut = null;
-            try {
-                fosErr = new FileOutputStream("Erros/Erros_Simulador");
-                fosOut = new FileOutputStream("Erros/Saida_Simulador");
-            } catch (FileNotFoundException ex) {
-                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            // define a impresso sobre os fluxos acima
-            PrintStream psErr = new PrintStream(fosErr);
-            PrintStream psOut = new PrintStream(fosOut);
-            // redefine os fluxos na classe System
-            //System.setErr(psErr);
-            //System.setOut(psOut);
-            try {
+        try {
+            // iSPD properties
+            Properties ispdProperties = new Properties();
+            // NOTE: 'ispd.properties' is located at resources/ directory
+            ispdProperties.load(Main.class.getResourceAsStream("/ispd.properties"));
+            // Set locale for running program
+            Locale.setDefault(new Locale(ispdProperties.getProperty("locale.language", "en"), ispdProperties.getProperty("locale.country", "US")));
+            if (args.length > 0) {
+                Terminal tel = new Terminal(args);
+                tel.executar();
+                System.exit(0);
+            } else {
+                // Configure loading program window
+                ImageIcon image = new ImageIcon(Main.class.getResource("/images/Splash.gif"));
+                SplashWindow window = new SplashWindow(image);
+                window.setText("Copyright (c) 2010 - 2014 GSPD.  All rights reserved.");
+                // Show loading program window
+                window.setVisible(true);
+                // Configure default log for carry exceptions during the program execution
+                LogExceptions logExceptions = new LogExceptions(null);
+                Thread.setDefaultUncaughtExceptionHandler(logExceptions);
+                // Errors files for simulator
+                // Get the error file paths
+                // TODO: if a directory does not exists, then create it
+                // NOTE: Currently, it assumes that path described in ispd.properties(directories.default) exists
+                File simulatorErrorFilePath = new File(ispdProperties.getProperty("directories.default", "."), "Erros_Simulador");
+                File outputSimulatorErrorFilePath = new File(ispdProperties.getProperty("directories.default", "."), "Saida_Simulador");
+                // Error files
+                FileOutputStream fosErr = new FileOutputStream(simulatorErrorFilePath);
+                FileOutputStream fosOut = new FileOutputStream(outputSimulatorErrorFilePath);
+                // Errors streams for simulator
+                PrintStream psErr = new PrintStream(fosErr);
+                PrintStream psOut = new PrintStream(fosOut);
+                // *** (need atention) ****
+                // redefine os fluxos na classe System
+                // System.setErr(psErr);
+                // System.setOut(psOut);
                 UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-            } catch (Exception ex) {
-                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                // Starts Graphical User Interface
+                JPrincipal gui = new JPrincipal();
+                gui.setLocationRelativeTo(null);
+                gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                logExceptions.setParentComponent(gui);
+                window.dispose();
+                gui.setVisible(true);
             }
-            JPrincipal gui = new JPrincipal();
-            gui.setLocationRelativeTo(null);
-            logExceptions.setParentComponent(gui);
-            window.dispose();
-            gui.setVisible(true);
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "File not Found", e);
+        } catch (NullPointerException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "Object not defined", e);
+        } catch (Exception e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "Something wrong has occurred while starting iSPD", e);
         }
     }
 }
