bd94277a8d61ca41e9ce0421681b7d75956122e9
Luís Baldissera <luis.baldissera26@gmail.com>
2019-06-08 13:30:47 -0300

    Automatic create home directory

--- src/main/java/gspd/ispd/IspdProperties.java
+++ src/main/java/gspd/ispd/IspdProperties.java
@@ -8,29 +8,41 @@ import java.util.Locale;
 import java.util.Properties;
 
 /**
- * IspdProperties
+ * This class has just static methods to access common properties of the iSPD.
+ * To load de configurantions, use {@code load} method, to edit them, see their setters,
+ * to retrieve see the getters, and to store, use {@code store} method.
  * 
- * This class has just static methods to access common properties of the iSPD
+ * @version 0.5
+ * @author luisbaldissera
  */
 public class IspdProperties {
 
-    private static final String propertiesName = "/ispd.properties";
+    private static final String PROPERTIES_FILE_NAME = "ispd.properties";
     
     private static Properties properties;
     private static Locale locale;
     private static Path workingDirectory;
 
     /**
-     * Load the configuration file of iSPD
+     * Load the configuration file of iSPD, ispd.properties ans control access and
+     * availability of its informations, as well as a simple way to store their
+     * possibly changes
      */
     public static void load() {
         try {
             properties = new Properties();
-            properties.load(Main.class.getResourceAsStream(propertiesName));
+            properties.load(Main.class.getResourceAsStream("/" + PROPERTIES_FILE_NAME));
             locale = new Locale(properties.getProperty("locale.language", "en"), properties.getProperty("locale.country", "US"));
-            setWorkingDirectory(System.getProperty("user.home") + File.separator +  properties.getProperty("directories.default", "."));
+            Locale.setDefault(locale);
+            setWorkingDirectory(getUserHome() + File.separator +  properties.getProperty("directories.default", "."));
+            File wdFile = workingDirectory.toFile();
+            if (!wdFile.exists()) {
+                wdFile.mkdir();
+            }
         } catch (IOException e) {
             System.err.println("Failed to open iSPD properties: " + e);
+        } catch (Exception e) {
+            System.err.println("An error occurred while loading properties: " + e);
         }
     }
 
@@ -42,16 +54,23 @@ public class IspdProperties {
     }
 
     /**
-     * @param workingDirectory the workingDirectory to set
+     * @param workingDirectory the working directory to set
      */
     public static void setWorkingDirectory(String workingDirectory) {
-        IspdProperties.workingDirectory = Paths.get(workingDirectory);
+        IspdProperties.workingDirectory = Paths.get(workingDirectory).normalize().toAbsolutePath();
     }
 
     /**
-     * @return the workingDirectory
+     * @return the working directory
      */
     public static String getWorkingDirectory() {
-        return workingDirectory.toAbsolutePath().toString();
+        return workingDirectory.toString();
+    }
+
+    /**
+     * @return the user home directory
+     */
+    public static String getUserHome() {
+        return System.getProperty("user.home");
     }
 }
--- src/main/java/gspd/ispd/Main.java
+++ src/main/java/gspd/ispd/Main.java
@@ -9,14 +9,9 @@ import gspd.ispd.gui.LogExceptions;
 import gspd.ispd.gui.SplashWindow;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.PrintStream;
-import java.net.URL;
-import java.util.Locale;
-import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
@@ -37,8 +32,6 @@ public class Main {
         try {
             // Load iSPD configurations
             IspdProperties.load();
-            // Set locale for running program
-            Locale.setDefault(IspdProperties.getLocale());
             if (args.length > 0) {
                 Terminal tel = new Terminal(args);
                 tel.executar();
@@ -55,9 +48,7 @@ public class Main {
                 Thread.setDefaultUncaughtExceptionHandler(logExceptions);
                 // Errors files for simulator
                 // Get the error file paths
-                // TODO: if a directory does not exists, then create it
-                // NOTE: Currently, it assumes that path described in ispd.properties(directories.default) exists
-                File simulatorErrorFilePath = new File(IspdProperties.getWorkingDirectory(), "Erros_Simulador");
+               File simulatorErrorFilePath = new File(IspdProperties.getWorkingDirectory(), "Erros_Simulador");
                 File outputSimulatorErrorFilePath = new File(IspdProperties.getWorkingDirectory(), "Saida_Simulador");
                 // Error files
                 FileOutputStream fosErr = new FileOutputStream(simulatorErrorFilePath);
