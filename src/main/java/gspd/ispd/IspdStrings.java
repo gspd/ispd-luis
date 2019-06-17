384b4012b7bc9adeb8adecea59c6e275a112af11
Luís Baldissera <luis.baldissera26@gmail.com>
2019-06-17 17:51:16 -0300

    Support n string replaces

--- src/main/java/gspd/ispd/IspdStrings.java
+++ src/main/java/gspd/ispd/IspdStrings.java
@@ -38,36 +38,37 @@ public class IspdStrings {
     /**
      * <p>
      * Get a string in the default language set in iSPD, and replaces substrings
-     * {@code $1 , $2 , ... , $n}, by the strings in 'replaces' respectively.
+     * <code> ${1} , ${2} , ... , ${n}</code>, by the strings in 'replaces' respectively.
      * </p>
      * </p>
      * <p>
      * Example: if a string is set in the string bundle as 
-     * {@code some.key=this is an $1 string}, this method should be called as
-     * {@code IspdString.get("some.key", "example");}, and it would return
-     * {@code "this is example string"}.
+     * <code>some.key=this is an ${1} string</code>, this method should be called as
+     * <code>IspdString.get("some.key", "example");</code>, and it would return
+     * <code>"this is example string"</code>.
      * </p>
      * <ul>
      *  <li>
-     *   <b>Notes:</b></p>
+     *   <p><b>Notes:</b></p>
      *   <ul>
-     *    <li>If the string has $i multiple times, then all them should be replaced.
-     *    Example: {@code other.key=Hi $1, you are a $1}, called as {@code IspdStringd.get("other.key", "friend")},
+     *    <li>If the string has <code>${i}</code> multiple times, then all of them
+     *    should be replaced.
+     *    <b>Example:</b> <code>other.key=Hi ${1}, you are a ${1}</code>, called as 
+     *    {@code IspdStringd.get("other.key", "friend")},
      *    would be replaced as {@code "Hi friend, you are a friend"}.
      *    </li>
-     *    <li>Currently, it just supports {@code $1 ... $9}.</li>
      *   </ul>
      *  </li>
      * </ul>
      * 
      * @param key the key of the string to be retrieved in the default language
-     * @param replaces the strings those are replace {@code $1 , $2 , ... , $n} , respectively
+     * @param replaces the strings those are replace <code>$1 , $2 , ... , $n</code> , respectively
      * @return the string with the iSPD set language replaced correctly
      */
     public static String get(String key, String ... replaces) {
         String string = stringsBundle.getString(key);
         for (int i = 1; i <= replaces.length; i++) {
-            string = string.replaceAll("\\$" + i, replaces[i-1]);
+            string = string.replaceAll("\\$\\{" + i  + "}", replaces[i-1]);
         }
         return string;
     }
