0a887120cf77b2fc9dc6ba320bc022adae0bf476
luisbaldissera <luis.baldissera26@gmail.com>
2020-01-28 10:00:36 -0300

    Removed Client{Input,Output}

--- src/main/java/gspd/ispd/queues/ServiceCentre.java
+++ src/main/java/gspd/ispd/queues/ServiceCentre.java
@@ -1,6 +1,12 @@
 package gspd.ispd.queues;
 
-public abstract class ServiceCentre<C extends Client> implements ClientInput<C>, ClientOutput<C> {
+public abstract class ServiceCentre<C extends Client> {
+
+    /**
+     * The multiplicity is the number of parallel servers a service
+     * centre has inside.
+     */
+    private int multiplicity;
 
     private EventObserver<FutureEvent<C>> clientRequestHandler;
     private EventObserver<FutureEvent<C>> clientEnteredHandler;
@@ -10,14 +16,24 @@ public abstract class ServiceCentre<C extends Client> implements ClientInput<C>,
     public void onClientRequest(EventObserver<FutureEvent<C>> handler) {
         this.clientRequestHandler = handler;
     }
+
     public void onClientEntered(EventObserver<FutureEvent<C>> handler) {
         this.clientEnteredHandler = handler;
     }
+
     public void onClientAttendance(EventObserver<FutureEvent<C>> handler) {
         this.clientAttendanceHandler = handler;
     }
+
     public void onClientExited(EventObserver<FutureEvent<C>> handler) {
         this.clientExitedHandler = handler;
     }
 
+    public void setMultiplicity(int multiplicity) {
+        this.multiplicity = multiplicity;
+    }
+
+    public int getMultiplicity() {
+        return multiplicity;
+    }
 }
